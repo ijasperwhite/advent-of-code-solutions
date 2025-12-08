1c925e7654b7e41820df55a9c6dc9c27e4250a1c
JasperWhite-Which <jasper.white@which.co.uk>
2025-12-08 10:52:32 +0000

    day7attemp

--- 2025-typescript/src/day07/day07.test.ts
+++ 2025-typescript/src/day07/day07.test.ts
@@ -1,13 +1,45 @@
 import { describe, it, expect } from "vitest";
-import { partOne, partTwo } from "./day07";
+import { explore, partOne, partTwo } from "./day07";
 import { fileContents } from "../utils/utils";
 
 describe("Day 07", () => {
+  describe("explore", () => {
+    it("should explore a simple list", () => {
+      const result = explore(
+        ["...S...", ".......", "..^....", "......."],
+        2,
+        0
+      );
+      expect(result).toBe(1);
+    });
+
+    it("should explore a simple list part 2", () => {
+      const result = explore(
+        ["...S...", ".......", "..^.^..", "......."],
+        4,
+        0
+      );
+      expect(result).toBe(1);
+    });
+
+    it("should explore a simple list part 3", () => {
+      const input = [
+        "...S...",
+        ".......",
+        "...^...",
+        ".......",
+        "..^.^..",
+        ".......",
+      ];
+      const result = explore(input, 3, 0);
+      expect(result).toBe(3);
+    });
+  });
   describe("Part 1", () => {
     it("should return result for example", () => {
       const input = fileContents("day07/example.txt");
       const result = partOne(input);
-      expect(result).toBe(0);
+      expect(result).toBe(21);
     });
 
     it("should return result for input", () => {
--- 2025-typescript/src/day07/day07.ts
+++ 2025-typescript/src/day07/day07.ts
@@ -1,8 +1,29 @@
+export const explore = (
+  rows: string[],
+  columnIndex: number,
+  rowIndex: number,
+  visited: Set<string>
+): Set<string> => {
+  for (let i = rowIndex + 1; i < rows.length; i++) {
+    if (rows[i].charAt(columnIndex) === "^") {
+      console.log("explore next", i, "on column  +/-", columnIndex);
+      visited.add(`${rowIndex}-${columnIndex}`);
+      const left = explore(rows, columnIndex - 1, i, visited);
+      const right = explore(rows, columnIndex + 1, i, visited);
+      return new Set([...visited, ...left, ...right]);
+    }
+  }
+  return visited; // base case
+};
+
 export const partOne = (s: string) => {
-  return 0;
+  const rows = s.split("\n");
+  const start = rows[0].indexOf("S");
+  console.log("starting at", start);
+  const visited = new Set<string>();
+  return explore(rows, start, 0, visited).size - 1;
 };
 
 export const partTwo = (s: string) => {
   return 0;
 };
-
