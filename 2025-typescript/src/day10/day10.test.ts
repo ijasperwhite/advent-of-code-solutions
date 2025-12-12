5655bce94644285ca8b309938e460866238b83c6
JasperWhite-Which <jasper.white@which.co.uk>
2025-12-12 10:05:55 +0000

    update day10

--- 2025-typescript/src/day10/day10.test.ts
+++ 2025-typescript/src/day10/day10.test.ts
@@ -1,5 +1,12 @@
 import { describe, it, expect } from "vitest";
-import { isAtTarget, partOne, partTwo, toInput } from "./day10";
+import {
+  isAtTarget,
+  partOne,
+  partTwo,
+  subsetsBitmask,
+  toButtonCombinations,
+  toInput,
+} from "./day10";
 import { fileContents } from "../utils/utils";
 
 describe("Day 10", () => {
@@ -11,6 +18,7 @@ describe("Day 10", () => {
       expect(result).toEqual({
         target: [0, 0, 0, 1, 0],
         curr: [0, 0, 0, 0, 0],
+        output: [7, 5, 12, 7, 2],
         buttons: [
           [1, 0, 1, 1, 1],
           [0, 0, 1, 1, 0],
@@ -33,8 +41,85 @@ describe("Day 10", () => {
     });
   });
 
+  describe("subsetsBitmask", () => {
+    it("should return all subsets", () => {
+      const input = [
+        [1, 0, 1, 1, 1],
+        [0, 0, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+      ];
+      const result = subsetsBitmask(input);
+      expect(result).toEqual([
+        [],
+        [[1, 0, 1, 1, 1]],
+        [[0, 0, 1, 1, 0]],
+        [
+          [1, 0, 1, 1, 1],
+          [0, 0, 1, 1, 0],
+        ],
+        [[1, 0, 0, 0, 1]],
+        [
+          [1, 0, 1, 1, 1],
+          [1, 0, 0, 0, 1],
+        ],
+        [
+          [0, 0, 1, 1, 0],
+          [1, 0, 0, 0, 1],
+        ],
+        [
+          [1, 0, 1, 1, 1],
+          [0, 0, 1, 1, 0],
+          [1, 0, 0, 0, 1],
+        ],
+      ]);
+    });
+  });
+
+  describe("toButtonCombinations", () => {
+    it("should return button combinations", () => {
+      const input = [
+        [1, 0, 1, 1, 1],
+        [0, 0, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+      ];
+      const result = toButtonCombinations(input);
+      expect(result).toEqual([
+        {
+          button: [1, 0, 1, 1, 1],
+          output: [1, 0, 1, 1, 1],
+          count: 1,
+        },
+        {
+          button: [0, 0, 1, 1, 0],
+          output: [0, 0, 1, 1, 0],
+          count: 1,
+        },
+        {
+          button: [1, 0, 0, 0, 1],
+          output: [1, 0, 2, 2, 1],
+          count: 2,
+        },
+        {
+          button: [1, 0, 0, 0, 1],
+          output: [1, 0, 0, 0, 1],
+          count: 1,
+        },
+        {
+          button: [0, 0, 1, 1, 0],
+          output: [2, 0, 1, 1, 2],
+          count: 2,
+        },
+        {
+          button: [1, 0, 1, 1, 1],
+          output: [1, 0, 1, 1, 1],
+          count: 2,
+        },
+      ]);
+    });
+  });
+
   describe("Part 1", () => {
-    it("should return result for example", () => {
+    it("should return result for example for part 1", () => {
       const input = fileContents("day10/example.txt");
       const result = partOne(input);
       expect(result).toBe(7);
@@ -43,15 +128,15 @@ describe("Day 10", () => {
     it("should return result for input", () => {
       const input = fileContents("day10/input.txt");
       const result = partOne(input);
-      expect(result).toBe(0);
+      expect(result).toBe(466);
     });
   });
 
   describe("Part 2", () => {
-    it("should return result for example", () => {
+    it("should return result for example for part 2", () => {
       const input = fileContents("day10/example.txt");
       const result = partTwo(input);
-      expect(result).toBe(0);
+      expect(result).toBe(33);
     });
 
     it("should return result for input", () => {
--- 2025-typescript/src/day10/day10.ts
+++ 2025-typescript/src/day10/day10.ts
@@ -1,16 +1,28 @@
 type Input = {
   curr: number[];
   target: number[];
+  output: number[];
   buttons: number[][];
 };
+
+type Result = {
+  count: number;
+  button: number[];
+  output: number[];
+};
+
 export const toInput = (s: string): Input => {
   const matchTarget = s.match(/\.|\#/g);
   const matchButtons = s.match(/\((.*?)\)/g);
+  const matchOutput = s.match(/\{(.*?)\}/g);
+
   const curr = matchTarget?.map((i) => 0) ?? [];
   const b = matchButtons?.map((i) => (i.match(/\d+/g) ?? []).map(Number)) ?? [];
   return {
     curr,
     target: matchTarget?.map((i) => (i === "." ? 0 : 1)) ?? [],
+    output:
+      matchOutput?.map((i) => i.match(/\d+/g)?.map(Number) ?? []).flat() ?? [],
     buttons: b.map((it) =>
       it.reduce((prev, next) => {
         const step = [...prev];
@@ -29,6 +41,14 @@ const getNextCurr = (curr: number[], button: number[]): number[] => {
   return result;
 };
 
+const getNextOutput = (output: number[], button: number[]): number[] => {
+  const result = [...output];
+  for (let i = 0; i < output.length; i++) {
+    result[i] = output[i] + button[i];
+  }
+  return result;
+};
+
 export const isAtTarget = (curr: number[], target: number[]): boolean => {
   for (let i = 0; i < curr.length; i++) {
     if (curr[i] !== target[i]) return false;
@@ -36,35 +56,105 @@ export const isAtTarget = (curr: number[], target: number[]): boolean => {
   return true;
 };
 
+export const isExceedOutput = (curr: number[], target: number[]): boolean => {
+  for (let i = 0; i < curr.length; i++) {
+    if (curr[i] > target[i]) return true;
+  }
+  return false;
+};
+
+export const subsetsBitmask = (nums: number[][]) => {
+  const n = nums.length;
+  const result = [];
+  for (let mask = 0; mask < 1 << n; mask++) {
+    const subset = [];
+    for (let i = 0; i < n; i++) {
+      if (mask & (1 << i)) subset.push(nums[i]);
+    }
+    result.push(subset);
+  }
+  return result;
+};
+
+export const toButtonCombinations = (b: number[][]) => {
+  const subsets = subsetsBitmask(b);
+  const result: Result[] = subsets.map((subset) => {
+    const none = b[0].map((_) => 0);
+    if (subset.length === 0) {
+      return { button: none, count: 0, output: none };
+    }
+    if (subset.length === 1) {
+      return { button: subset[0], count: 1, output: subset[0] };
+    }
+    const multi = subset.reduce(
+      (prev, next) => {
+        return {
+          button: getNextCurr(prev.button, next),
+          count: prev.count + 1,
+          output: getNextOutput(prev.output, next),
+        };
+      },
+      { button: none, count: 0, output: none }
+    );
+    return multi;
+  });
+  return result.filter((i) => i.button.some((j) => 1 === j));
+};
+
 export const partOne = (s: string) => {
   const rows = s.split("\n").map(toInput);
   const results = rows.map((r) => {
-    // try using a bfs
-    const buttons = r.buttons;
-    const queue = [];
-    queue.push(r.curr);
-    let n = 0;
-    let size = 1;
-    while (queue.length > 0) {
-      n++;
-      while (size > 0) {
-        const next = [...queue.shift()!];
-        for (let b of buttons) {
-          const nextCurr = getNextCurr(next, b);
-          if (isAtTarget(nextCurr, r.target)) {
-            return n;
-          }
-          queue.push(nextCurr);
-        }
-        size--;
+    const { target, buttons } = r;
+    let min = buttons.length + 1;
+
+    const combo = toButtonCombinations(buttons);
+    console.log(combo);
+    for (let i = 0; i < combo.length; i++) {
+      if (isAtTarget(combo[i].button, target) && combo[i].count < min) {
+        min = combo[i].count;
       }
-      size = queue.length;
     }
-    return n;
+    return min;
   });
   return results.reduce((sum, next) => sum + next, 0);
 };
 
 export const partTwo = (s: string) => {
+  const rows = s.split("\n").map(toInput);
+  const results = rows.map((r) => {
+    const { target, buttons, output } = r;
+    let min = output.reduce((prev, next) => prev * next) * buttons.length;
+
+    const combo = toButtonCombinations(buttons);
+    // console.log("combo length", combo.length);
+
+    const queue: Result[] = [];
+    combo.forEach((i) => queue.push(i)); // assume none exceed limit
+    while (queue.length > 0) {
+      const next = queue.shift()!;
+
+      if (
+        isAtTarget(next.button, target) &&
+        isAtTarget(next.output, output) &&
+        min > next.count
+      ) {
+        min = next.count;
+      }
+
+      for (let i = 0; i < combo.length; i++) {
+        const nextButton = getNextCurr(next.button, combo[i].button);
+        const nextOutput = getNextOutput(next.output, combo[i].output);
+
+        if (!isExceedOutput(nextOutput, output)) {
+          queue.push({
+            button: nextButton,
+            output: nextOutput,
+            count: next.count + combo[i].count,
+          });
+        }
+      }
+    }
+    return min;
+  });
   return 0;
 };
